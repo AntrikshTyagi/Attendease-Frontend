262a687a0d7dd913fb3e12c2cfaba97b925b4147
AntrikshTyagi <antriksh.tyagi55@gmail.com>
2023-01-28 22:31:55 +0530

    complete files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import UploadButton from "./UploadButton";
+// import Button from '@mui/material/Button';
+import './SimpleContainer'
+// import SimpleContainer from "./SimpleContainer";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+      <SignIn />
+      <SignUp />
+      {/* <SignUp /> */}
+      {/* <SimpleContainer> */}
+        {/* <UploadButton /> */}
+      {/* </SimpleContainer> */}
+    </div>
+  );
+}
+
+export default App;
