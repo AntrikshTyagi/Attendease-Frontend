22de87c0e15061dc2303aa6c0fedede8a831ad37
DivyanshuParwal <divyanshuparwal2001@gmail.com>
2023-02-13 12:34:17 +0530

    update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,58 +1,262 @@
-import React, { useState } from "react";
-import {
-  AppBar,
-  Button,
-//   Tab,
-  Tabs,
-  Toolbar,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import AddBusinessRoundedIcon from "@mui/icons-material/AddBusinessRounded";
-import DrawerComp from "./Drawer";
-const Header = () => {
-  const [value, setValue] = useState();
-  const theme = useTheme();
-  console.log(theme);
-  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
-  console.log(isMatch);
+// import React, { useState } from "react";
+// import { Link } from 'react-router-dom';
+
+// import { styled } from "@mui/material/styles";
+// import { purple } from "@mui/material/colors";
+// import {
+//   AppBar,
+//   Button,
+//   //   Tab,
+//   Tabs,
+//   Toolbar,
+//   Typography,
+//   useMediaQuery,
+//   useTheme,
+// } from "@mui/material";
+// import AddBusinessRoundedIcon from "@mui/icons-material/AddBusinessRounded";
+// import DrawerComp from "./Drawer";
+
+// const ColorButton = styled(Button)(({ theme }) => ({
+//   // color: theme.palette.getContrastText(purple[500]),
+//   // backgroundColor: "inherit",
+//   marginLeft: "10px",
+//   textTransform: "uppercase",
+//   "&:hover": {
+//     color: "inherit",
+//     background: "rgba(200, 200, 200, 0.2)",
+//     boxShadow: "none",
+//   },
+// }));
+
+// const Header = () => {
+//   const [value, setValue] = useState();
+//   const theme = useTheme();
+//   console.log(theme);
+//   const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+//   console.log(isMatch);
+
+//   return (
+//     <React.Fragment>
+//       <AppBar sx={{ background: "#512da8" }}>
+//         <Toolbar>
+//           <AddBusinessRoundedIcon sx={{ transform: "scale(2)" }} />
+//           {isMatch ? (
+//             <>
+//               <Typography sx={{ fontSize: "2rem", paddingLeft: "10%" }}>
+//                 Facial Attendance
+//               </Typography>
+//               <DrawerComp />
+//             </>
+//           ) : (
+//             <>
+//               <Tabs
+//                 sx={{ marginLeft: "auto" }}
+//                 indicatorColor="secondary"
+//                 textColor="inherit"
+//                 value={value}
+//                 onChange={(e, value) => setValue(value)}
+//               >
+//                 <h2>Facial Attendance</h2>
+//               </Tabs>
+//               <ColorButton color="inherit">About</ColorButton>
+//               <ColorButton color="inherit">Contact</ColorButton>
+//               {/* <ColorButton variant="contained">Custom CSS</ColorButton> */}
+//               {/* <Button
+//                 sx={{ marginLeft: "10px", background: "#e9c46a" }}
+//                 variant="contained"
+//               >
+//                 SignUp
+//               </Button> */}
+//             </>
+//           )}
+//         </Toolbar>
+//       </AppBar>
+//     </React.Fragment>
+//   );
+// };
+
+// export default Header;
+
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+
+const pages = ["About", "Contact"];
+// const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
-    <React.Fragment>
-      <AppBar sx={{ background: "#fb8500" }}>
-        <Toolbar>
-          <AddBusinessRoundedIcon sx={{ transform: "scale(2)" }} />
-          {isMatch ? (
-            <>
-              <Typography sx={{ fontSize: "2rem", paddingLeft: "10%" }}>
-                Facial Attendance
-              </Typography>
-              <DrawerComp />
-            </>
-          ) : (
-            <>
-              <Tabs
-                sx={{ marginLeft: "auto" }}
-                indicatorColor="secondary"
-                textColor="inherit"
-                value={value}
-                onChange={(e, value) => setValue(value)}
+    <AppBar position="static" sx={{ background: "#512da8" }}>
+      <Container maxWidth="xl">
+        <Toolbar
+          disableGutters
+          sx={{ display: "flex", justifyContent: "space-between" }}
+        >
+          {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            FACIAL ATTENDANCE
+          </Typography>
+          <Box
+            sx={{
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
-                <h2>Facial Attendance</h2>
-              </Tabs>
-              <Button sx={{ marginLeft: "auto", background: "#e9c46a"}} onFocus={{background: "#fb8500"}} variant="contained">
-                Login
+                {page}
               </Button>
-              <Button sx={{ marginLeft: "10px", background: "#e9c46a" }} variant="contained">
-                SignUp
-              </Button>
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
-  );
-};
+            ))}
+          </Box>
 
-export default Header;
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            FACIAL ATTENDANCE
+          </Typography>
+          {/* <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box> */}
+
+          {/* <Box sx={{ flexGrow: 0 }}> */}
+          {/* <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu> */}
+
+          {/* </Box> */}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
