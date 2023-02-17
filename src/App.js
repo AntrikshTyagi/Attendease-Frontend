33317fad493696840bc8a752138a924a364e7256
DivyanshuParwal <divyanshuparwal2001@gmail.com>
2023-02-17 20:40:10 +0530

    update SignIn.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
 // import UploadButton from "./UploadButton";
 // import Button from '@mui/material/Button';
-import './SimpleContainer'
+import "./SimpleContainer";
 import Header from "./Header";
 // import SimpleContainer from "./SimpleContainer";
-// import SignIn from "./SignIn";
-// import SignUp from "./SignUp";
-
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Header />
+        <Header />
       </header>
       {/* <Header /> */}
-      {/* <SignIn /> */}
+      <SignIn />
       {/* <SignUp /> */}
       {/* <SignUp /> */}
       {/* <SimpleContainer> */}
-        {/* <UploadButton /> */}
+      {/* <UploadButton /> */}
       {/* </SimpleContainer> */}
     </div>
   );
